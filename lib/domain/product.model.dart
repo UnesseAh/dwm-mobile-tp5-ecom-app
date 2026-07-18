0d9a629c7e5ef3e22fe765ea9160870b37da7bbe
UnesseAh <youness.ahasla@gmail.com>
2026-07-18 13:40:15 +0100

    initial project and create product class

---
+++ lib/domain/product.model.dart
@@ -0,0 +1,16 @@
+class Product {
+  final int id;
+  final String name;
+  final double price;
+  final int quantity;
+  final String photo;
+  final bool selected;
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.quantity,
+    required this.photo,
+    required this.selected,
+  });
+}
