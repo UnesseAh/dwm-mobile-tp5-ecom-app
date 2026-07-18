8ece456fc73cefc5392004c32bd1b76ca9d8ff86
UnesseAh <youness.ahasla@gmail.com>
2026-07-18 14:03:21 +0100

    create products list

---
+++ lib/data/products.repository.dart
@@ -0,0 +1,30 @@
+import 'package:ecom_app/domain/product.model.dart';
+
+class ProductsRepository {
+  final List<Product> products = [
+    Product(
+      id: 1,
+      name: "Computer-1",
+      price: 3200,
+      quantity: 2,
+      photo: "images/Laptop-computer.webp",
+      selected: false,
+    ),
+    Product(
+      id: 1,
+      name: "Computer-2",
+      price: 1200,
+      quantity: 5,
+      photo: "images/Laptop-computer.webp",
+      selected: false,
+    ),
+    Product(
+      id: 1,
+      name: "Computer-3",
+      price: 200,
+      quantity: 90,
+      photo: "images/Laptop-computer.webp",
+      selected: false,
+    ),
+  ];
+}
--- lib/domain/product.model.dart
+++ lib/domain/product.model.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class Product {
   final int id;
   final String name;
@@ -13,4 +14,22 @@ class Product {
     required this.photo,
     required this.selected,
   });
+
+  Product copyWith({
+    int? id,
+    String? name,
+    double? price,
+    int? quantity,
+    String? photo,
+    bool? selected,
+  }) {
+    return Product(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      price: price ?? this.price,
+      quantity: quantity ?? this.quantity,
+      photo: photo ?? this.photo,
+      selected: selected ?? this.selected,
+    );
+  }
 }
